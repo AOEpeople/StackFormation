6842bff491cd237e7f24afe06918501164556506
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-01-13 17:45:43 +0000

    Introducing dryrun

--- src/StackFormation/Command/DeployCommand.php
+++ src/StackFormation/Command/DeployCommand.php
@@ -25,28 +25,42 @@ class DeployCommand extends AbstractCommand
                 'o',
                 InputOption::VALUE_NONE,
                 'Observe stack after'
+            )
+            ->addOption(
+                'dryrun',
+                'd',
+                InputOption::VALUE_NONE,
+                'Dry run'
             );
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
     {
+        $dryRun = $input->getOption('dryrun');
+        if ($dryRun) {
+            $formatter = new \Symfony\Component\Console\Helper\FormatterHelper();
+            $formattedBlock = $formatter->formatBlock(['Dry Run!'], 'error', true);
+            $output->writeln("\n$formattedBlock\n");
+        }
         $this->interactAskForConfigStack($input, $output);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $dryRun = $input->getOption('dryrun');
         $stack = $input->getArgument('stack');
-        $this->stackManager->deployStack($stack);
+        $this->stackManager->deployStack($stack, $dryRun);
 
-        $effectiveStackName = $this->stackManager->getConfig()->getEffectiveStackName($stack);
+        if (!$dryRun) {
+            $effectiveStackName = $this->stackManager->getConfig()->getEffectiveStackName($stack);
+            $output->writeln("Triggered deployment of stack '$effectiveStackName'.");
 
-        $output->writeln("Triggered deployment of stack '$effectiveStackName'.");
-
-        if ($input->getOption('observe')) {
-            return $this->stackManager->observeStackActivity($effectiveStackName, $output);
-        } else {
-            $output->writeln("\n-> Run this to observe the stack creation/update:");
-            $output->writeln("{$GLOBALS['argv'][0]} stack:observe $effectiveStackName\n");
+            if ($input->getOption('observe')) {
+                return $this->stackManager->observeStackActivity($effectiveStackName, $output);
+            } else {
+                $output->writeln("\n-> Run this to observe the stack creation/update:");
+                $output->writeln("{$GLOBALS['argv'][0]} stack:observe $effectiveStackName\n");
+            }
         }
     }
 }
