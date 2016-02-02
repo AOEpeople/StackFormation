5b56f04d0a888e07a5776e7bba41522ae0812e4c
Lee Saferite <lee.saferite@aoe.com>
2016-02-02 09:59:45 -0500

    Formatting

--- src/StackFormation/TemplateMerger.php
+++ src/StackFormation/TemplateMerger.php
@@ -40,28 +40,49 @@ class TemplateMerger
                 $prefix = $key;
 
                 // Update all { "Ref": "..." }
-                $template = preg_replace_callback('/\{\s*"Ref"\s*:\s*"([a-zA-Z0-9:]+?)"\s*\}/', function ($matches) use ($prefix) {
-                    return '{"Ref":"' . $prefix . $matches[1] . '"}';
-                }, $template);
+                $template = preg_replace_callback(
+                    '/\{\s*"Ref"\s*:\s*"([a-zA-Z0-9:]+?)"\s*\}/',
+                    function ($matches) use ($prefix) {
+                        return '{"Ref":"' . $prefix . $matches[1] . '"}';
+                    },
+                    $template
+                );
 
                 // Update all { "DependsOn": "..." }
-                $template = preg_replace_callback('/\"DependsOn"\s*:\s*"([a-zA-Z0-9:]+?)"/', function ($matches) use ($prefix) {
-                    return '"DependsOn":"' . $prefix . $matches[1] . '"';
-                }, $template);
+                $template = preg_replace_callback(
+                    '/\"DependsOn"\s*:\s*"([a-zA-Z0-9:]+?)"/',
+                    function ($matches) use ($prefix) {
+                        return '"DependsOn":"' . $prefix . $matches[1] . '"';
+                    },
+                    $template
+                );
 
                 // Update all { "DependsOn": ["...", "...", ...] }
-                $template = preg_replace_callback('/\"DependsOn"\s*:\s*\[(.*)\]/s', function ($matches) use ($prefix) {
-                    $dependencies = $matches[1];
-                    $dependencies = preg_replace_callback('/"([a-zA-Z0-9:]+?)"/', function ($matches) use ($prefix) {
-                        return '"'.$prefix.$matches[1].'"';
-                    }, $dependencies);
-                    return '"DependsOn":[' . $dependencies . ']';
-                }, $template);
+                $template = preg_replace_callback(
+                    '/\"DependsOn"\s*:\s*\[(.*)\]/s',
+                    function ($matches) use ($prefix) {
+                        $dependencies = $matches[1];
+                        $dependencies = preg_replace_callback(
+                            '/"([a-zA-Z0-9:]+?)"/',
+                            function ($matches) use ($prefix) {
+                                return '"' . $prefix . $matches[1] . '"';
+                            },
+                            $dependencies
+                        );
+
+                        return '"DependsOn":[' . $dependencies . ']';
+                    },
+                    $template
+                );
 
                 //  Update all "Fn::GetAtt": ["...", "..."] }
-                $template = preg_replace_callback('/\"Fn::GetAtt"\s*:\s*\[s*"([a-zA-Z0-9:]+?)"/', function ($matches) use ($prefix) {
-                    return '"Fn::GetAtt": ["' . $prefix . $matches[1] . '"';
-                }, $template);
+                $template = preg_replace_callback(
+                    '/\"Fn::GetAtt"\s*:\s*\[s*"([a-zA-Z0-9:]+?)"/',
+                    function ($matches) use ($prefix) {
+                        return '"Fn::GetAtt": ["' . $prefix . $matches[1] . '"';
+                    },
+                    $template
+                );
             }
 
             $array = json_decode($template, true);
@@ -71,7 +92,7 @@ class TemplateMerger
             foreach ($topLevelKeys as $topLevelKey) {
                 if (isset($array[$topLevelKey])) {
                     foreach ($array[$topLevelKey] as $key => $value) {
-                        $newKey = $prefix.$key;
+                        $newKey = $prefix . $key;
                         if (isset($mergedTemplate[$topLevelKey][$newKey])) {
                             throw new \Exception("Duplicate key '$newKey' found in '$topLevelKey'");
                         }
