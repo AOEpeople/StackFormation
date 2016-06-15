cf1c785cb3384c19bb68e7aa0f3a7bc1f72c2497
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-06-14 23:56:54 -0400

    Fixed path to autoloader

--- tests/phpunit.bootstrap.php
+++ tests/phpunit.bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
-$loader = require __DIR__ . '/../../../../vendor/autoload.php'; /* @var $loader \Composer\Autoload\ClassLoader */
+$i = 0;
+do {
+    $autoloader = __DIR__ . str_repeat('/..', $i) . '/vendor/autoload.php';
+    $i++;
+} while ($i < 6 && !is_file($autoloader));
+
+$loader = require $autoloader; /* @var $loader \Composer\Autoload\ClassLoader */
 $loader->setUseIncludePath(true);
 define('FIXTURE_ROOT', __DIR__ . '/fixtures/');
