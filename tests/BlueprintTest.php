ee02034174c6cf7798047f1dcd8e0b29540b17a3
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-06-15 22:51:25 -0400

    Added failing tests (TDD! :)

--- tests/BlueprintTest.php
+++ tests/BlueprintTest.php
@@ -16,4 +16,51 @@ class BlueprintTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('BlueprintBar', $blueprintVars['BlueprintFoo']);
     }
 
+    /**
+     * @test
+     */
+    public function getConditionalParameterValueDefault()
+    {
+        $config = new \StackFormation\Config([FIXTURE_ROOT.'/Config/blueprint.conditional_value.yml']);
+        $cfnClientMock = $this->getMock('\Aws\CloudFormation\CloudFormationClient', [], [], '', false);
+        $resolverMock = $this->getMock('\StackFormation\PlaceholderResolver', [], [], '', false);
+        $blueprintFactory = new \StackFormation\BlueprintFactory($cfnClientMock, $config, $resolverMock);
+        $blueprint = $blueprintFactory->getBlueprint('conditional_value');
+        $parameters = $blueprint->getParameters(true, true);
+        $this->assertArrayHasKey('CondValue', $parameters);
+        $this->assertEquals('c', $parameters['CondValue']);
+    }
+
+    /**
+     * @test
+     */
+    public function getConditionalParameterValueEnvFooVal1()
+    {
+        putenv('Foo=Val1');
+        $config = new \StackFormation\Config([FIXTURE_ROOT.'/Config/blueprint.conditional_value.yml']);
+        $cfnClientMock = $this->getMock('\Aws\CloudFormation\CloudFormationClient', [], [], '', false);
+        $resolverMock = $this->getMock('\StackFormation\PlaceholderResolver', [], [], '', false);
+        $blueprintFactory = new \StackFormation\BlueprintFactory($cfnClientMock, $config, $resolverMock);
+        $blueprint = $blueprintFactory->getBlueprint('conditional_value');
+        $parameters = $blueprint->getParameters(true, true);
+        $this->assertArrayHasKey('CondValue', $parameters);
+        $this->assertEquals('a', $parameters['CondValue']);
+    }
+
+    /**
+     * @test
+     */
+    public function getConditionalParameterValueEnvFooVal2()
+    {
+        putenv('Foo=Val2');
+        $config = new \StackFormation\Config([FIXTURE_ROOT.'/Config/blueprint.conditional_value.yml']);
+        $cfnClientMock = $this->getMock('\Aws\CloudFormation\CloudFormationClient', [], [], '', false);
+        $resolverMock = $this->getMock('\StackFormation\PlaceholderResolver', [], [], '', false);
+        $blueprintFactory = new \StackFormation\BlueprintFactory($cfnClientMock, $config, $resolverMock);
+        $blueprint = $blueprintFactory->getBlueprint('conditional_value');
+        $parameters = $blueprint->getParameters(true, true);
+        $this->assertArrayHasKey('CondValue', $parameters);
+        $this->assertEquals('b', $parameters['CondValue']);
+    }
+
 }
