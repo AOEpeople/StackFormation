2ac462b4ed29663f1dd99f7e51f9d26be2a509ad
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-06-20 17:05:49 -0400

    Added info to message

--- src/StackFormation/Blueprint.php
+++ src/StackFormation/Blueprint.php
@@ -259,7 +259,7 @@ class Blueprint {
             $reference = base64_encode($encodedValues);
         }
         if (strlen($reference) > 255) {
-            throw new \Exception('Blueprint reference too long (even after compression)');
+            throw new \Exception('Blueprint reference too long (even after compression): ' . strlen($reference) . ' chars');
         }
         return $reference;
     }
