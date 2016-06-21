7b2d18c75d6a45638fceb951e4dc4e3fd65a27bc
Fabrizio Branca <fabrizio.branca@aoe.com>
2016-06-20 22:23:56 -0400

    Experimenting with the tree view

--- src/StackFormation/Command/Stack/ListCommand.php
+++ src/StackFormation/Command/Stack/ListCommand.php
@@ -30,8 +30,43 @@ class ListCommand extends \StackFormation\Command\AbstractCommand
             );
     }
 
+    /**
+     * Render the tree and return it as a string.
+     *
+     * @return string|null
+     */
+    public function render(array $data)
+    {
+        $output = '';
+        $treeIterator = new \RecursiveTreeIterator(new \RecursiveArrayIterator($data), \RecursiveTreeIterator::SELF_FIRST);
+        foreach ($treeIterator as $val) {
+            $val = str_replace('-Array', '-\\', $val);
+            $output .= $val . "\n";
+        }
+        return $output;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $nameFilter = $input->getOption('nameFilter');
+        $statusFilter = $input->getOption('statusFilter');
+
+        $stacks = $this->stackFactory->getStacksFromApi(false, $nameFilter, $statusFilter);
+        $data = [];
+        foreach ($stacks as $stackName => $stack) { /* @var $stack Stack */
+            $stackNameParts = explode('-', $stackName, 5);
+            $c = count($stackNameParts);
+            if ($c == 1) { $data[$stackNameParts[0]] = $stackName; }
+            if ($c == 2) { $data[$stackNameParts[0]][$stackNameParts[1]] = $stackName; }
+            if ($c == 3) { $data[$stackNameParts[0]][$stackNameParts[1]][$stackNameParts[2]] = $stackName; }
+            if ($c == 4) { $data[$stackNameParts[0]][$stackNameParts[1]][$stackNameParts[2]][$stackNameParts[3]] = $stackName; }
+            if ($c >= 5) { $data[$stackNameParts[0]][$stackNameParts[1]][$stackNameParts[2]][$stackNameParts[3]][$stackNameParts[4]] = $stackName; }
+        }
+
+        $output->writeln($this->render($data));
+
+        return;
+
         $nameFilter = $input->getOption('nameFilter');
         $statusFilter = $input->getOption('statusFilter');
 
