1a98961752f3daf19d755a3dc84fb8e77de0900d
Julian Kleinhans <julian.kleinhans@aoe.com>
2016-12-13 15:59:25 +0100

    #45 Add ExpandCidrIp stage for tree preprocessor

---
+++ src/StackFormation/PreProcessor/Stage/Tree/ExpandCidrIp.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace StackFormation\PreProcessor\Stage\Tree;
+
+use StackFormation\PreProcessor\Stage\AbstractTreePreProcessorStage;
+use StackFormation\PreProcessor\Rootline;
+
+class ExpandCidrIp extends AbstractTreePreProcessorStage
+{
+
+    /**
+     * This one will resolve comma-separated list of IP addresses
+     *
+     * @param string $path
+     * @param string $value
+     * @param Rootline $rootLineReferences
+     * @return bool
+     */
+    function invoke($path, $value, Rootline $rootLineReferences) {
+        if (!preg_match('+/SecurityGroupIngress/.*/CidrIp+', $path) || strpos($value, ',') === false) {
+            return false; // indicate that nothing has been touched
+        }
+
+        $parentRootlineItem = $rootLineReferences->parent(1); /* @var $parentRootlineItem RootlineItem */
+        $parent = $parentRootlineItem->getValue(); /* @var $parent RecursiveArrayObject */
+
+        $grandParentRootlineItem = $rootLineReferences->parent(2); /* @var $grandParentRootlineItem RootlineItem */
+        $grandParent = $grandParentRootlineItem->getValue(); /* @var $grandParent RecursiveArrayObject */
+
+        // remove original item
+        $grandParent->offsetUnset($parentRootlineItem->getKey());
+
+        // add a new line for every csl item
+        foreach (explode(',', $value) as $cidrIp) {
+            $newItem = clone $parent;
+            $newItem->CidrIp = $cidrIp;
+            $grandParent->append($newItem);
+        }
+
+        return true; // indicate that something has changed
+    }
+}
