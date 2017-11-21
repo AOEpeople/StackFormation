16a6cee8bbb9ac23a5d70606838a91964b6a6d0d
Fabrizio Branca <fabrizio.branca@aoe.com>
2017-11-21 11:10:56 +0100

    Allow skip reading .env or .env.default file if env var SF_SKIP_DOTENV is true

--- src/dotenv.php
+++ src/dotenv.php
@@ -2,13 +2,15 @@
 
 define('CWD', getcwd());
 
-if (is_readable(CWD . DIRECTORY_SEPARATOR . '.env.default')) {
-    $dotenv = new Dotenv\Dotenv(CWD, '.env.default');
-    $dotenv->overload();
-}
-if (is_readable(CWD . DIRECTORY_SEPARATOR . '.env')) {
-    $dotenv = new Dotenv\Dotenv(CWD);
-    $dotenv->overload();
+if (!getenv('SF_SKIP_DOTENV')) {
+    if (is_readable(CWD . DIRECTORY_SEPARATOR . '.env.default')) {
+        $dotenv = new Dotenv\Dotenv(CWD, '.env.default');
+        $dotenv->overload();
+    }
+    if (is_readable(CWD . DIRECTORY_SEPARATOR . '.env')) {
+        $dotenv = new Dotenv\Dotenv(CWD);
+        $dotenv->overload();
+    }
 }
 
 register_shutdown_function(function() {
